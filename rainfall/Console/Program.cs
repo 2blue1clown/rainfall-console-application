c1c0c1b6b3e3a09db38bfb7e8382c58e520414e7
bb-33 <jonolj56@gmail.com>
2023-11-22 15:33:10 +0000

    read in file and print data

--- rainfall/Console/Program.cs
+++ rainfall/Console/Program.cs
@@ -1,5 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using FileService;
+using CoreService.Models.DeviceData;
+using CoreService.Models.RainfallData;
+
+
+
+
+
+var dict = new Dictionary<string, List<RainfallData>>();
+
+var device_path = "C:\\Users\\61421\\Documents\\webdev-2023\\rainfall\\data\\Devices.csv";
+var data_path = "C:\\Users\\61421\\Documents\\webdev-2023\\rainfall\\data\\Data1.csv";
+
+FileService.DataReader<DeviceData, RainfallData>.LoadDataToDictionaryKeys(device_path, "Id", dict);
+FileService.DataReader<DeviceData, RainfallData>.LoadDataToDictionaryValues(data_path, "Id", dict);
+
+Printer<RainfallData>.Print(dict);
+
 
 
