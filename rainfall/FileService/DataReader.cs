b62e9c70794a6c02991c3f7e8d32de94e61879ca
bb-33 <jonolj56@gmail.com>
2023-11-22 14:56:09 +0000

    fixed types on DataReader

--- rainfall/FileService/DataReader.cs
+++ rainfall/FileService/DataReader.cs
@@ -4,9 +4,9 @@ using CsvHelper;
 
 namespace FileService;
 
-public class DataReader<T, K> where T : new()
+public class DataReader<T>
 {
-    public Dictionary<K, T> LoadDataToDictionary(string path, string keyProp, Dictionary<K, T> dict)
+    public static Dictionary<string, List<T>> LoadDataToDictionaryKeys(string path, string keyProp, Dictionary<string, List<T>> dict)
     {
         using (var reader = new StreamReader(path))
         using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
@@ -17,14 +17,40 @@ public class DataReader<T, K> where T : new()
             foreach (var row in data)
             {
                 PropertyInfo propInfo = row.GetType().GetProperty(keyProp);
-                K key = (K)propInfo.GetValue(row, null);
+                string key = (string)propInfo.GetValue(row, null);
 
                 if (!dict.ContainsKey(key))
                 {
-                    dict.Add(key, new T());
+                    dict.Add(key, new List<T>());
                 }
             }
         }
         return dict;
     }
+
+    public static Dictionary<string, List<T>> LoadDataToDictionaryValues(string path, string keyProp, Dictionary<string, List<T>> dict)
+    {
+
+        using (var reader = new StreamReader(path))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            // read device data and prepare for sorting of data
+            var data = csv.GetRecords<T>();
+
+            foreach (var row in data)
+            {
+                PropertyInfo propInfo = row.GetType().GetProperty(keyProp);
+                string key = (string)propInfo.GetValue(row, null);
+
+                if (!dict.ContainsKey(key))
+                {
+                    dict[key].Add(row);
+                }
+            }
+        }
+
+        return dict;
+    }
+
+
 }
