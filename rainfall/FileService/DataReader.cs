70d1e4cb5805cef03404d18e51a2aca6e363001f
bb-33 <jonolj56@gmail.com>
2023-11-22 15:32:27 +0000

    Messed around with types on datareader

--- rainfall/FileService/DataReader.cs
+++ rainfall/FileService/DataReader.cs
@@ -4,9 +4,9 @@ using CsvHelper;
 
 namespace FileService;
 
-public class DataReader<T>
+public class DataReader<T, V>
 {
-    public static Dictionary<string, List<T>> LoadDataToDictionaryKeys(string path, string keyProp, Dictionary<string, List<T>> dict)
+    public static Dictionary<string, List<V>> LoadDataToDictionaryKeys(string path, string keyProp, Dictionary<string, List<V>> dict)
     {
         using (var reader = new StreamReader(path))
         using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
@@ -21,31 +21,29 @@ public class DataReader<T>
 
                 if (!dict.ContainsKey(key))
                 {
-                    dict.Add(key, new List<T>());
+                    dict.Add(key, new List<V>());
                 }
             }
         }
         return dict;
     }
 
-    public static Dictionary<string, List<T>> LoadDataToDictionaryValues(string path, string keyProp, Dictionary<string, List<T>> dict)
+    public static Dictionary<string, List<V>> LoadDataToDictionaryValues(string path, string keyProp, Dictionary<string, List<V>> dict)
     {
 
         using (var reader = new StreamReader(path))
         using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
         {
             // read device data and prepare for sorting of data
-            var data = csv.GetRecords<T>();
+            var data = csv.GetRecords<V>();
 
             foreach (var row in data)
             {
                 PropertyInfo propInfo = row.GetType().GetProperty(keyProp);
                 string key = (string)propInfo.GetValue(row, null);
 
-                if (!dict.ContainsKey(key))
-                {
-                    dict[key].Add(row);
-                }
+                dict[key].Add(row);
+
             }
         }
 
