0f4125bb03e99ee6a525649d887275b15af01991
bb-33 <jonolj56@gmail.com>
2023-11-23 10:22:50 +0000

    avgs

--- rainfall/CoreService/Processor.cs
+++ rainfall/CoreService/Processor.cs
@@ -1,6 +1,25 @@
-﻿namespace CoreService;
+﻿using CoreService.Models.RainfallData;
+
+namespace CoreService;
 
 public class Processor
 {
+    private static double RainfallAvg(List<RainfallData> data)
+    {
+        double total = 0;
+        data.ForEach(row => total += row.Rainfall);
+        return total / data.Count;
+    }
+
+    public static List<Tuple<string, double>> RainfallAvgs(Dictionary<string, List<RainfallData>> dict)
+    {
+        var lst = new List<Tuple<string, double>>();
+        foreach (var key in dict.Keys)
+        {
+            lst.Add(new Tuple<string, double>(key, RainfallAvg(dict[key])));
+        }
+        return lst;
+    }
+
 
 }
