e5c7048b5cf9320c6c2a2b95b627fd63fe532f39
bb-33 <jonolj56@gmail.com>
2023-11-23 11:27:26 +0000

    calculate recent averages instead

--- rainfall/Console/Program.cs
+++ rainfall/Console/Program.cs
@@ -1,10 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using CoreService;
 using CoreService.Models.DeviceData;
 using CoreService.Models.RainfallData;
-
-
-var dict = new Dictionary<string, List<RainfallData>>();
 
 var device_path = "C:\\Users\\61421\\Documents\\webdev-2023\\rainfall\\data\\Devices.csv";
 var data_folder = "C:\\Users\\61421\\Documents\\webdev-2023\\rainfall\\data";
@@ -17,5 +15,8 @@ FileService.DataReader.LoadFileDataToList(device_path, device_data);
 
 var sorted_data = HelperService.Helper.SortIntoDictionary(device_data, rainfall_data);
 
-Printer.PrintDictionary(sorted_data);
+// Printer.PrintDictionary(sorted_data);
+var processor = new Processor(sorted_data);
+var recent_avgs = processor.RecentRainfallAvgs();
+recent_avgs.ForEach(tup => Console.WriteLine("{0} has avg reading of {1} in the last 4 hours", tup.Item1, tup.Item2));
 
--- rainfall/CoreService/Processor.cs
+++ rainfall/CoreService/Processor.cs
@@ -1,24 +1,55 @@
-﻿using CoreService.Models.RainfallData;
+﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using CoreService.Models.RainfallData;
 
 namespace CoreService;
 
 public class Processor
 {
-    private static double RainfallAvg(List<RainfallData> data)
+    Dictionary<string, List<RainfallData>> dict;
+    DateTime currentTime;
+
+    public Processor(Dictionary<string, List<RainfallData>> dict)
+    {
+        this.dict = dict;
+        this.setCurrentTime();
+    }
+
+    private void setCurrentTime()
+    {
+        var data = dict.Values.ToList();
+        this.currentTime = DateTime.MinValue;
+        data.ForEach(subData => subData.ForEach(row =>
+        {
+            if (DateTime.Compare(currentTime, row.Time) < 0)
+            {
+                this.currentTime = row.Time;
+            }
+        }));
+    }
+
+    private static double Avg(List<int> data)
     {
         double total = 0;
-        data.ForEach(row => total += row.Rainfall);
+        data.ForEach(num => total += num);
         return total / data.Count;
     }
 
-    public static List<Tuple<string, double>> RainfallAvgs(Dictionary<string, List<RainfallData>> dict)
+    public List<Tuple<string, double>> RecentRainfallAvgs()
     {
         var lst = new List<Tuple<string, double>>();
-        foreach (var key in dict.Keys)
+        var cutoff = this.currentTime.AddHours(-4);
+        foreach (var key in this.dict.Keys)
         {
-            lst.Add(new Tuple<string, double>(key, RainfallAvg(dict[key])));
+            var data = this.OnlyDataAfter(this.dict[key], cutoff).Select(row => row.Rainfall).ToList();
+            lst.Add(new Tuple<string, double>(key, Avg(data)));
         }
         return lst;
+    }
+
+    private List<RainfallData> OnlyDataAfter(List<RainfallData> lst, DateTime cutoff)
+    {
+        return lst.Where(row => DateTime.Compare(cutoff, row.Time) < 0).ToList();
     }
 
 
