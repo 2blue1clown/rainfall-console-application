3fe8182b8c1f917a06bc7f64a764fc00f019e5f0
bb-33 <jonolj56@gmail.com>
2023-11-23 10:03:01 +0000

    adjusted printer

--- rainfall/Console/Printer.cs
+++ rainfall/Console/Printer.cs
@@ -1,7 +1,7 @@
 
-public class Printer<T>
+public class Printer
 {
-    public static void Print(Dictionary<string, List<T>> dict)
+    public static void PrintDictionary<T>(Dictionary<string, List<T>> dict)
     {
         foreach (var key in dict.Keys)
         {
