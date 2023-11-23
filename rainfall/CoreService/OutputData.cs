e859b5a1310d4fd689f01c2f702bc1fea2331a2f
bb-33 <jonolj56@gmail.com>
2023-11-23 12:42:42 +0000

    added trend

---
+++ rainfall/CoreService/OutputData.cs
@@ -0,0 +1,21 @@
+
+namespace CoreService.Models.OutputData;
+
+public enum Classification { GREEN, RED, AMBER }
+public enum Trend { INCREASING, DECREASING, FLAT }
+
+
+public class OutputData : IId
+{
+    public string Id { get; set; }
+    public double Avg { get; set; }
+    public Classification Classification { get; set; }
+
+    public Trend Trend { get; set; }
+
+    public override string ToString()
+    {
+        return string.Format("Device Id: {0} Classification: {1} Avg Reading in Last 4 Hours: {2} Trend: {3}", Id, Classification.ToString(), Avg, Trend.ToString());
+    }
+
+}
