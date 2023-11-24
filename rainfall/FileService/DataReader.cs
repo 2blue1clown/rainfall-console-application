276dc8df7b003914dfb98b97812a4b5529953926
bb-33 <jonolj56@gmail.com>
2023-11-24 08:46:04 +0000

    exploring using IEnumerable and LINQ rather than list objects

--- rainfall/FileService/DataReader.cs
+++ rainfall/FileService/DataReader.cs
@@ -6,6 +6,31 @@ namespace FileService;
 
 public class DataReader
 {
+    public static IEnumerable<T> LoadFileData<T>(string path)
+    {
+        using (
+                var reader = new StreamReader(path)
+                )
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            // read device data and prepare for sorting of data
+            var data = csv.GetRecords<T>();
+            if (data != null) return data;
+
+        }
+        return Enumerable.Empty<T>();
+    }
+
+    public static IEnumerable<T> LoadFolderData<T>(string path)
+    {
+        var collection = Enumerable.Empty<T>();
+        foreach (string file in Directory.EnumerateFiles(path))
+        {
+            collection.Concat(LoadFileData<T>(file));
+        }
+        return collection;
+    }
+
 
     /// <summary>
     /// Puts data from file into given list
