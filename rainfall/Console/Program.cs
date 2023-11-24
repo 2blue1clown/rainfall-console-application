a4143ad16a399b97dfc4b0946080c973ccefae31
bb-33 <jonolj56@gmail.com>
2023-11-24 14:30:25 +0000

    use any instead of count

--- rainfall/Console/Program.cs
+++ rainfall/Console/Program.cs
@@ -16,19 +16,18 @@ var data_folder = args[1];
 var rainfall_data = FileService.DataReader.LoadFolderData<RainfallData>(data_folder);
 var device_data = FileService.DataReader.LoadFileData<DeviceData>(device_path);
 
-if (device_data.Count() == 0)
+if (!device_data.Any())
 {
     Console.WriteLine("ERROR: No devices provided in the devices data file");
     Environment.Exit(1);
 }
 
-if (rainfall_data.Count() == 0)
+if (!rainfall_data.Any())
 {
     Console.WriteLine("ERROR: No valid rainfall data provided in folder");
     Environment.Exit(1);
 }
 
-// Printer.PrintDictionary(sorted_data);
 Console.WriteLine();
 var processor = new Processor(rainfall_data, device_data);
 foreach (var report in processor.reportData)
