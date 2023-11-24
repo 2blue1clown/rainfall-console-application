ffce39078dd87910fe23a2fdc49f06ffef39e3e4
bb-33 <jonolj56@gmail.com>
2023-11-24 13:05:23 +0000

    added name and location to the report

--- rainfall/CoreService/ReportData.cs
+++ rainfall/CoreService/ReportData.cs
@@ -10,6 +10,8 @@ public enum Trend { INCREASING, DECREASING, FLAT }
 public class ReportData : IId
 {
     public string Id { get; set; }
+    public string Location { get; set; }
+    public string Name { get; set; }
     public double Avg { get; set; }
     public Classification Classification { get; set; }
 
@@ -17,7 +19,7 @@ public class ReportData : IId
 
     public override string ToString()
     {
-        return string.Format("Device Id: {0}\nClassification: {1}\nAverage Reading in Last 4 Hours: {2}\nTrend: {3}\n\n", Id, Classification.ToString(), Avg, Trend.ToString());
+        return string.Format("Name: {4}\nDevice Id: {0}\nLocation: {5}\nClassification: {1}\nAverage Reading in Last 4 Hours: {2}\nTrend: {3}\n\n", Id, Classification.ToString(), Avg, Trend.ToString(), Name, Location);
     }
 
 }
